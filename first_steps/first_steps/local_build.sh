c0bb3952e03836d4f62e585c055a775419b08447
Lautaro Santillan <santillanlautaro03@gmail.com>
2025-11-19 00:14:00 -0300

    build.sh -v8

--- first_steps/first_steps/local_build.sh
+++ first_steps/first_steps/local_build.sh
@@ -3,7 +3,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init 
-API_URL=http://app.first-steps reflex export --frontend-only
+reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
