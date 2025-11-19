a9a212e66a940a3e79ec57b8067dcb19e10bf477
Lautaro Santillan <santillanlautaro03@gmail.com>
2025-11-18 22:46:10 -0300

    Ver deploy con Github Actions -v4

--- first_steps/rxconfig.py
+++ first_steps/rxconfig.py
@@ -2,6 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="first_steps",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://first-steps"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
